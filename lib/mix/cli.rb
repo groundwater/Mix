8f7ec833bbca4457b939ede1cae155a5fd124d2b
Jacob Groundwater <groundwater@gmail.com>
2013-04-28 11:05:42 -0700

    First Draft

--- lib/mix/cli.rb
+++ lib/mix/cli.rb
@@ -19,6 +19,12 @@ eoh
   
   def initialize(command,target)
     case command
+    when 'clone'
+      clone target
+    when 'bootstrap'
+      bootstrap target
+    when 'start'
+      start target
     when 'install'
       install target
     else
@@ -27,20 +33,65 @@ eoh
     end
   end
   
+  def pathify(target)
+    "#{ENV['HOME']}/Documents/Mix/Github/#{target}"
+  end
+  
   def resolve(target)
     if /\//.match target
       "git@github.com:#{target}.git"
     end
   end
   
+  def dependencies(target)
+    dest = pathify target
+    deps = File.read File.join(dest,'Envfile')
+    out = {}
+    deps.each_line do |line|
+      split = line.split /\s*:\s*/
+      lhs = split[0]
+      rhs = split[1].strip!
+      out[lhs] = rhs
+    end
+    out
+  end
+  
+  def bootstrap(target)
+    puts "Bootstrapping Target #{target}"
+    dest = pathify target
+    %w{install}.each do |line|
+      puts "--> Running Task #{line}"
+      out = `(cd #{dest} && cat Taskfile | grep ^#{line} | sed 's/^#{line}\\s*:\\s*//' | bash | sed 's/^/    /')`
+    end
+  end
+  
+  def start(target)
+    dest = pathify target
+    port = 9999
+    system "(cd #{dest} && cat Procfile | head -n 1 | sed 's/.*:\\w*//' | PORT=#{port} bash)"
+  end
+  
   def install(target)
+    clone target
+    (dependencies target).each do |key,value|
+      puts "Target #{target} Had Dependency #{key} --> #{value}"
+      install value
+    end
+    bootstrap target
+  end
+  
+  def clone(target)
     url = resolve target
     if url
       puts "Target Resolved to #{url}"
-      dest = "~/Documents/Mix/Github/#{target}"
-      puts "Installing Target to #{dest}"
-      `mkdir -p #{dest}`
-      `git clone #{url} #{dest}`
+      dest = pathify target
+      if File.directory? dest
+        puts "Target Already Cloned to #{dest}"
+      else
+        puts "Installing Target to #{dest}"
+        `mkdir -p #{dest}`
+        `git clone #{url} #{dest} | sed 's/^/    /'`
+      end
     else
       puts "Unable to resolve target #{target}"
     end
